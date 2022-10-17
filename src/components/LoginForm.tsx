a5a94bf7043d236e0681877a532eb2164a99b3dc
stanislav <shilov@scioffice.com>
2022-10-18 01:23:26 +0700

    create Form

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,20 +1,57 @@
-import { Form } from "antd";
+import { Button, Checkbox, Form, Input } from "antd";
 import React, { FC } from "react";
 
 const LoginForm: FC = () => {
-	const [form] = Form.useForm();
+	const onFinish = (values: any) => {
+		console.log("Success:", values);
+	};
+
+	const onFinishFailed = (errorInfo: any) => {
+		console.log("Failed:", errorInfo);
+	};
 	return (
-		<Form form={form}>
+		<Form
+			name="basic"
+			labelCol={{ span: 8 }}
+			wrapperCol={{ span: 16 }}
+			initialValues={{ remember: true }}
+			onFinish={onFinish}
+			onFinishFailed={onFinishFailed}
+			autoComplete="off"
+			className="LoginForm"
+		>
 			<Form.Item
-				label="Username: "
+				label="Username"
 				name="username"
-				rules={[{ required: true, message: "Please input your username" }]}
-			/>
+				rules={[{ required: true, message: "Please input your username!" }]}
+			>
+				<Input />
+			</Form.Item>
+
 			<Form.Item
-				label="Passwor: "
+				label="Password"
 				name="password"
-				rules={[{ required: true, message: "Please input your pasword" }]}
-			/>
+				rules={[{ required: true, message: "Please input your password!" }]}
+			>
+				<Input.Password />
+			</Form.Item>
+
+			<Form.Item
+				name="remember"
+				valuePropName="checked"
+				wrapperCol={{ offset: 8, span: 16 }}
+			>
+				<Checkbox>Remember me</Checkbox>
+			</Form.Item>
+
+			<Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+				<Button
+					type="primary"
+					htmlType="submit"
+				>
+					Submit
+				</Button>
+			</Form.Item>
 		</Form>
 	);
 };
