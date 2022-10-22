99361d3c6f115cff719ea745510c149d5edc39e9
stanislav <shilov@scioffice.com>
2022-10-22 23:41:02 +0700

    create custom hook - useTypedSelector

--- src/hooks/useTypedSelector.ts
+++ src/hooks/useTypedSelector.ts
@@ -1,6 +1,6 @@
 // Типизированный селектор
 
-import { RootState } from './../store/index';
+import { RootState } from '../store/store';
 import { TypedUseSelectorHook, useSelector } from "react-redux";
 
 export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
