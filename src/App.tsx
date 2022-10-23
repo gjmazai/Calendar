12ba034890537da057d6f5fa5a4d231e819f4f0a
stanislav <shilov@scioffice.com>
2022-10-24 00:05:30 +0700

    check whether the user is registered or not

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { Layout } from "antd";
 import AppRouter from "./components/AppRouter";
 import NavBar from "./components/NavBar";
 
 import "./App.css";
+import { useActions } from "./hooks/useActions";
+import { IUser } from "./models/IUser";
 
 function App() {
+	const { setUser, setIsAuth } = useActions();
+	useEffect(() => {
+		if (localStorage.getItem("auth")) {
+			setUser({ username: localStorage.getItem("username" || "") } as IUser);
+			setIsAuth(true);
+		}
+	}, []);
+
 	return (
 		<Layout>
 			<NavBar />
@@ -19,3 +29,4 @@ function App() {
 }
 
 export default App;
+
