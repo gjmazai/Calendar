74f555b0ef149b48d99e14436426c3696b756fc4
stanislav <shilov@scioffice.com>
2022-10-24 00:09:01 +0700

    deleting a comment

--- src/store/reducer/auth/index.ts
+++ src/store/reducer/auth/index.ts
@@ -9,22 +9,6 @@ const initialState: AuthState = {
 	user: {} as IUser
 }
 
-// export default function authReducer(state = initialState, action: AuthAction): AuthState{
-// 	switch(action.type){
-// 		case ActionEnum.SET_AUTH:
-// 			return {...state, isAuth: action.payload, isLoading:false}
-// 		case ActionEnum.SET_ERROR:
-// 			return {...state, error: action.payload, isLoading: false}
-// 		case ActionEnum.SET_IS_LOADING:
-// 			return {...state, isLoading: action.payload}
-// 		case ActionEnum.SET_USER:
-// 			return {...state, user: action.payload}
-			
-// 		default:
-// 			return state;
-// 	} 
-// }
-
 export const authSlice = createSlice({
 	name: 'auth',
 	initialState,
@@ -41,6 +25,7 @@ export const authSlice = createSlice({
 				state.isLoading = action.payload;
 			},
 			setUser(state, action: PayloadAction<IUser>){
+				debugger;
 				state.user = action.payload;
 			}
 	}
