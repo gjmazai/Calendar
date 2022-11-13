49d4bbeeec126ed1408893f04d85233a82b8f443
stanislav <shilov@scioffice.com>
2022-11-13 18:21:43 +0700

    render events in calendar

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -1,13 +1,28 @@
-import { Calendar } from "antd";
+import { Badge, Calendar } from "antd";
+import { Moment } from "moment";
 import React, { FC } from "react";
 import { IEvent } from "../models/IEvent";
+import { formatDate } from "../utils/date";
 
 interface EventCalendarProp {
 	events: IEvent[];
 }
 
-const EventCalendar: FC<EventCalendarProp> = () => {
-	return <Calendar />;
+const EventCalendar: FC<EventCalendarProp> = (props) => {
+	function dateCellRender(value: Moment){
+		const formatedDate = formatDate(value.toDate());
+		const currentDayEvents = props.events.filter(ev => ev.date === formatedDate)
+		return(
+			<div>
+				{currentDayEvents.map((el, index) => 
+					<div key={index}>
+						{el.description}
+					</div>
+				)}
+			</div>
+		)
+	}
+	return <Calendar dateCellRender={dateCellRender}/>;
 };
 
 export default EventCalendar;
