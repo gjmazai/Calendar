3bdf291c9932e22f3b4d5749b02bb4961a579270
stanislav <shilov@scioffice.com>
2022-11-13 18:23:47 +0700

    add new reducer and actions

--- src/store/RootReducer.ts
+++ src/store/RootReducer.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import authReducer from "./reducer/auth/index";
+import eventReducer from "./reducer/event/index";
 
 export const RootReducer = combineReducers({
 	authReducer,
+	eventReducer,
 })
--- src/store/allActionsCreator.ts
+++ src/store/allActionsCreator.ts
@@ -1,7 +1,11 @@
+import { EventActionCreator } from './reducer/event/actions-creators';
+import { eventSlice } from './reducer/event/index';
 import {AuthActionCreator} from './reducer/auth/action-creator';
 import {authSlice} from './reducer/auth';
 
 export const allActionsCreator  = {
 	...AuthActionCreator,
 	...authSlice.actions,
+	...eventSlice.actions,
+	...EventActionCreator,
 }
