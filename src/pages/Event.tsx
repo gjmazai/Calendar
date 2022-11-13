5741553a2e58f10cad28f2ff04d8733f59f20c94
stanislav <shilov@scioffice.com>
2022-11-13 18:24:09 +0700

    fix bags

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,12 +1,29 @@
 import { Button, Layout, Modal, Row } from "antd";
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import EventCalendar from "../components/EventCalendar";
+import EventForm from "../components/EventForm";
+import { useActions } from "../hooks/useActions";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+import { IEvent } from "../models/IEvent";
 
 const Event: FC = () => {
 	const [visible, setVisible] = useState(false);
+	const { fetchGuests, createEvent, fetchEvents } = useActions();
+	const { guests, event} = useTypedSelector((state) => state.eventReducer);
+	const {user} = useTypedSelector(state => state.authReducer);
+
+	useEffect(() => {
+		fetchGuests();
+		fetchEvents(user.username);
+	}, []);
+
+	const addNewEvents = (event: IEvent) => {
+		setVisible(false);
+		createEvent(event);
+	}
 	return (
 		<Layout>
-			<EventCalendar events={[]} />
+			<EventCalendar events={event} />
 			<Row justify="center">
 				<Button onClick={() => setVisible(true)}>Add event</Button>
 			</Row>
@@ -15,10 +32,14 @@ const Event: FC = () => {
 				open={visible}
 				footer={null}
 				onCancel={() => setVisible(false)}
-			></Modal>
+			>
+				<EventForm 
+				guests={guests}
+				submit={addNewEvents}
+				/>
+			</Modal>
 		</Layout>
 	);
 };
 
 export default Event;
-
